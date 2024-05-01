e62b277fc683528925b44e7b09aa9a3293c6efe2
WerctFourth <ntrvr05@gmail.com>
2024-05-01 18:03:17 +0500

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-# python-border-autocrop
+# What is this?
 A Python script that crops borders from images.
+![Example](res/example.jpg)
+# Features
+* Reads JPEGs and PNGs
+* Saves to PNG and AVIF
+* Works on color and grayscale images
+* Processes a single file or a whole directory recursively
+* Resizes to fit a certain width
+* Adjustable thresholds for noise insusceptibility
+* Multi-core processing
+* Auto-detects a grayscale image and saves it as such
+# Requirements
+Python, Pillow and Numpy. The current version has been tested with Python 3.12.2, [Pillow](https://pypi.org/project/pillow/) 10.3.0 and [Numpy](https://pypi.org/project/numpy/) 1.26.4. It may work on other versions.
+
+[External AVIF encoder](https://github.com/AOMediaCodec/libavif) is required to save images to AVIF.
+# How to use
+```
+usage: autocrop.py [-h] -i INPUT -o OUTPUT [-fr FILLRATIO] [-cd COLORDISTANCE] [-et EXHAUSTIVETHRESHOLD]
+                          [-rct RGBCOLORTHRESHOLD] [-nr] [-rf] [-rw RESIZEWIDTH] [-rh RESIZEHEIGHT]
+                          [-vrt VERTICALRESIZETARGET] [-hrt HORIZONTALRESIZETARGET] [-ea] [-aep AVIFEXECUTABLEPATH]
+                          [-eq ENCODEQUALITY] [-pcl PNGCOMPRESSIONLEVEL] [-lp LOGPATH] [-nw]
+
+Semi-automated border cropper
+
+options:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        Input file or directory
+  -o OUTPUT, --output OUTPUT
+                        Output directory template
+  -fr FILLRATIO, --fillratio FILLRATIO
+                        Border fill ratio in % (0.0 to 100.0, default=0.4)
+  -cd COLORDISTANCE, --colordistance COLORDISTANCE
+                        How many colors to treat as a background color (0 to 255, default=16)
+  -et EXHAUSTIVETHRESHOLD, --exhaustivethreshold EXHAUSTIVETHRESHOLD
+                        How much of a border must be cropped to not trigger exhaustive pass in % (0.0 to 100.0,
+                        default=-1, disabled=-1, always=0)
+  -rct RGBCOLORTHRESHOLD, --rgbcolorthreshold RGBCOLORTHRESHOLD
+                        How much difference between bands is allowed to save a RGB image as grayscale (0 to 255,
+                        default=9, always=255)
+  -nr, --noresize       Disable resizing
+  -rf, --resizefit      Enable fit resizing instead of fit only width
+  -rw RESIZEWIDTH, --resizewidth RESIZEWIDTH
+                        Target width for fit resizing
+  -rh RESIZEHEIGHT, --resizeheight RESIZEHEIGHT
+                        Target height for fit resizing
+  -vrt VERTICALRESIZETARGET, --verticalresizetarget VERTICALRESIZETARGET
+                        Width resize target for vertical images (default=1200)
+  -hrt HORIZONTALRESIZETARGET, --horizontalresizetarget HORIZONTALRESIZETARGET
+                        Width resize target for horizontal images (default=1920)
+  -ea, --encodeavif     Enable encoding to AVIF
+  -aep AVIFEXECUTABLEPATH, --avifexecutablepath AVIFEXECUTABLEPATH
+                        Path to Avifenc executable (default=script directory)
+  -eq ENCODEQUALITY, --encodequality ENCODEQUALITY
+                        Encode quality (0 to 100, default=54)
+  -pcl PNGCOMPRESSIONLEVEL, --pngcompressionlevel PNGCOMPRESSIONLEVEL
+                        PNG compression level (0 to 9, default=1)
+  -lp LOGPATH, --logpath LOGPATH
+                        Log file path (default=script directory/autocroplog.txt)
+  -nw, --nowait         Do not wait for a key press after completion
+  ```
+## Important settings
+### Cropping
+The options `-fr` (fill ratio), `-cd` (color distance), and `-et` (exhaustive pass threshold) are used to determine how aggressive the cropping will be performed. In essence, increasing `-fr` may help with high contrast noise on a border, `-cd` - with low contrast noise, `-et` (disabled by default) - with sporadic noise only on some lines or in the very beginning of an image.
+
+If your images have clean borders, you can try to leave these settings on defaults and check results, they're quite safe on images.
+
+If you have noisy borders, then you may have to increase these values slightly, check the results and adjust them to get desired images. Don't increase then too much as very high values will corrupt the results very easily.
+
+If you want to know more of what exactly these parameters do, please read the "How does it work?" section.
+### Resizing
+The resizing resolution can be controlled using the -nr (disables resizing) -vrt (sets the target width for a vertical/portrait image) -hrt (sets the target width for a horizontal/landscape image). The resizing algorihm implemented now is not a "fit" one, but something like "fit width for a rotating screen". So for example for `-vrt 1200` and `-hrt 1920` a source `2000x3000` image will become a `1200x1800` image, and `3000x2000` will become `1920x1280`.
+
+For a more traditional "fit" resizing enable the `-rf` switch and set the desired resolution using the `-rw` (for width) and `-rh` (for height) arguments.
+## Examples
+Process a directory `R:\Input` with default thresholds and save files to `R:\ResultPng` and `R:\ResultAvif`; use avifenc.exe from a script's directory, save log to a script's directory `autocroplog.txt`:
+
+`python autocrop.py -i R:\Input -o R:\Result -ea`
+
+Process a single file `R:\test.png` with default thresholds, don't encode to AVIF, don't resize, save to `R:\OutputPng`, save log to `R:\alog.txt`:
+
+`python autocrop.py -i R:\test.png -o R:\Output -lp R:\alog.txt -nr`
+
+Process a directory `R:\Input`, set fill ratio to 2%, set color distance to 32 colors, set exaustive pass threshold to 5%, save only in PNG to `R:\SavesPng`, don't wait on exit:
+
+`python autocrop.py -i R:\Input, -o R:\Saves -fr 2 -cd 32 -et 5 -nw`
+
+Process `R:\test.png`, fit the image to `1920x1080`, don't encode to AVIF and save to `R:\SavePng`:
+
+`python autocrop.py -i R:\test.png -o R:\Save -rf -rw 1920 -rh 1080`
+# How does it work?
+## Cropping
+The script loads an image and converts it into a grayscale/luminance-only form. Then it starts processing it line-by-line (row or coulumn, depends on what side of an image is being cropped).
+
+It counts every unique luminance value in the line and how many pixels uses it. The most used luma value becomes a background luma value. But luma values that are neighbors to the background value are treated similarly: this is controlled by the `-cd` setting (by an absolute value). So if 255 was determined to be the background value, then (if `-cd` is set to 16) all luma values from 239 to 255 are treated as background. And if the background is 128, then the background range is 112-144. This may help to reduce errors from JPEG artifacts and low contrast noise.
+
+Then the script counts all pixels in the line that have a luma value that belongs to the background range. Then it checks whether the portion of non-background pixels exceeds the "fill ratio" (`-fr` setting, may help with high contrast noise or unnecessary details, such as page numbers) percentage or not. If it doesn't, then the line still belongs to the border and the script starts checking the next line. If it does, then the scripts stops analyzing at this line and crops the detected border.
+
+Left, right, top and bottom sides are analyzed separately from an outer egde towards center.
+
+The `-et` option enables the exhaustive cropping mode (it functions on left and right borders only currently). If percentage of cropped lines is below the `-et` value, then the script starts the exhaustive pass. The difference from the previous pass is now the script doesn't stop on a first line that exceeds the fill ratio. Instead, it proceeds to analyze all lines.
+## Saving
+The `-o` option sets a template for output directories. Then the script generates the directory names by adding `Png` for PNGs and `Avif` for AVIFs. Also, the script creates output directories for the last subdirectory of an input file. For example, if `-o` is set to `R:\Output`, `-i` is set to `R:\Input`, and the file `R:\Input\SomeDir1\SomeDir2\test.png` is processed, then the file will be saved as `R:\OutputPng\SomeDir2\test.png` as a PNG and `R:\OutputAvif\SomeDir2\test.png` as an AVIF.
+
+## AVIF encoding
+This script uses an external AVIF encoder because the Pillow AVIF Plugin was always outdated and was restricted to multithreaded processing (the script forces avifenc to use a single thread). After the script has saved an image in PNG, it passes parameters to the avifenc using the saved PNG as source, executes it and waits for it to complete.
+# Suggestions / bugs?
+Please open an issue if you have an idea (maybe good heurisics to automatically set parameters for each image?😁) or spotted a bug.
---
+++ autocrop.py
@@ -0,0 +1,307 @@
+from PIL import Image, ImageChops, ImageCms
+import os, pathlib, multiprocessing, subprocess, logging, numpy, argparse, io
+    
+def getAvifCmdline(argInPath: pathlib.Path, argOutPath: pathlib.Path, argPBlock) -> list:
+    tmpList = list()
+    tmpList.append(argPBlock.avifexecutablepath.as_posix())
+    tmpList.append("-s")
+    tmpList.append("3")
+    tmpList.append("-q")
+    tmpList.append(str(argPBlock.encodequality))
+    tmpList.append("-j")
+    tmpList.append("1")
+    tmpList.append(argInPath.as_posix())
+    tmpList.append(argOutPath.as_posix())
+    return tmpList
+
+def getParameterBlock():
+    parser = argparse.ArgumentParser(description="Semi-automated border cropper")
+    parser.add_argument("-i", "--input", type=pathlib.Path, help="Input file or directory", required=True)
+    parser.add_argument("-o", "--output", type=pathlib.Path, help="Output directory template", required=True)
+    parser.add_argument("-fr", "--fillratio", default=0.4, type=float, help="Border fill ratio in %% (0.0 to 100.0, default=0.4)")
+    parser.add_argument("-cd", "--colordistance", default=16, type=int, help="How many colors to treat as a background color (0 to 255, default=16)")
+    parser.add_argument("-et", "--exhaustivethreshold", default=-1, type=float, help="How much of a border must be cropped to not trigger exhaustive pass in %% (0.0 to 100.0, default=-1, disabled=-1, always=0)")
+    parser.add_argument("-rct", "--rgbcolorthreshold", default=9, type=int, help="How much difference between bands is allowed to save a RGB image as grayscale (0 to 255, default=9, always=255)")
+    parser.add_argument("-nr", "--noresize", action="store_true", help="Disable resizing")
+    parser.add_argument("-rf", "--resizefit", action="store_true", help="Enable fit resizing instead of fit only width")
+    parser.add_argument("-rw", "--resizewidth", default=0, type=int, help="Target width for fit resizing")
+    parser.add_argument("-rh", "--resizeheight", default=0, type=int, help="Target height for fit resizing")
+    parser.add_argument("-vrt", "--verticalresizetarget", default=1200, type=int, help="Width resize target for vertical images (default=1200)")
+    parser.add_argument("-hrt", "--horizontalresizetarget", default=1920, type=int, help="Width resize target for horizontal images (default=1920)")
+    parser.add_argument("-ea", "--encodeavif", action="store_true", help="Enable encoding to AVIF")
+    parser.add_argument("-aep", "--avifexecutablepath", type=pathlib.Path, default="./avifenc", help="Path to Avifenc executable (default=script directory)")
+    parser.add_argument("-eq", "--encodequality", default=54, type=int, help="Encode quality (0 to 100, default=54)")
+    parser.add_argument("-pcl", "--pngcompressionlevel", default=1, type=int, help="PNG compression level (0 to 9, default=1)")
+    parser.add_argument("-lp", "--logpath", default="./autocroplog.txt", type=pathlib.Path, help="Log file path (default=script directory/autocroplog.txt)")
+    parser.add_argument("-nw", "--nowait", action="store_true", help="Do not wait for a key press after completion")
+    args = parser.parse_args()
+
+    if args.fillratio < 0 or args.fillratio > 100:
+        print("fillratio must be 0.0 to 100.0")
+        exit()
+    if args.colordistance < 0 or args.colordistance > 255:
+        print("colordistance must be 0 to 255")
+        exit()
+    if args.exhaustivethreshold < -1 or args.exhaustivethreshold > 100:
+        print("exhaustivethreshold must be -1 to 100.0")
+        exit()
+    if args.rgbcolorthreshold < 0 or args.rgbcolorthreshold > 255:
+        print("rgbcolorthreshold must be 0 to 255")
+        exit()
+    if args.verticalresizetarget <= 0:
+        print("verticalresizetarget must be >0")
+        exit()
+    if args.horizontalresizetarget <= 0:
+        print("horizontalresizetarget must be >0")
+        exit()       
+    if args.pngcompressionlevel < 0 or args.pngcompressionlevel > 9:
+        print("pngcompressionlevel must be 0 to 9")
+        exit()
+    if not pathlib.Path.exists(args.input):
+        print("input file or directory must exist")
+        exit()
+    if args.encodeavif and not pathlib.Path.exists(args.avifexecutablepath):
+        print("avifexecutablepath must point to valid avifenc")
+        exit()
+    if args.encodeavif and (args.encodequality < 0 or args.encodequality > 100):
+        print("encodequality must be 0 to 100")
+        exit()
+    if args.resizefit and (args.resizewidth <= 0 or args.resizeheight <= 0):
+        print("For resizefit: resizewidth and resizeheight must be set and >0")
+        exit()
+    return args
+
+def savePng(argImg: Image, argPath: pathlib.Path, argPBlock):
+    if argImg.mode == "1" or argImg.mode == "L" or argImg.mode == "LA" or argImg.mode == "P":
+        argImg.save(argPath, "png", compress_level=argPBlock.pngcompressionlevel)
+    else:
+        if checkColor(argImg):
+            if argImg.mode == "RGB" or argImg.mode == "I" or argImg.mode == "RGBA":
+                argImg.save(argPath, "png", compress_level=argPBlock.pngcompressionlevel)
+            else:
+                argImg.convert(mode="RGB", dither=Image.Dither.NONE).save(argPath, "png", compress_level=argPBlock.pngcompressionlevel)
+        else:
+            argImg.convert(mode="L", dither=Image.Dither.NONE).save(argPath, "png", compress_level=argPBlock.pngcompressionlevel)
+
+def checkColor(argImg: Image, argPBlock) -> bool:
+    imgBands = argImg.split()
+    return ImageChops.difference(imgBands[0], imgBands[1]).getextrema()[1] > argPBlock.rgbcolorthreshold or \
+            ImageChops.difference(imgBands[1], imgBands[2]).getextrema()[1] > argPBlock.rgbcolorthreshold
+
+def getResultFilePath(argSourceFilePath: pathlib.Path, argExtension: str, argPBlock) -> pathlib.Path:
+    return argPBlock.output.with_name(argPBlock.output.name + argExtension.title()) / argSourceFilePath.parent.name \
+          / (argSourceFilePath.stem + "." + argExtension.lower())
+
+def fuzzyCount(argSourceDict: dict, argPBlock) -> int:
+    colorList = list(argSourceDict.keys())
+    tmpValue = 0
+    tmpMainColor = int(colorList[0])
+    for currentColor in colorList:
+        if abs(tmpMainColor - int(currentColor)) < argPBlock.colordistance:
+            tmpValue += argSourceDict[currentColor]
+    return tmpValue
+        
+def getResampleSize(argX: int, argY: int, argPBlock):
+    if argPBlock.resizefit:
+        tmpRatio = min(argPBlock.resizewidth / argX, argPBlock.resizeheight / argY)
+        return (argX * tmpRatio, argY * tmpRatio)
+    else:
+        if argX < argY: #Image is vertical
+            internalTarget = argPBlock.verticalresizetarget
+        else: #Image is horizontal
+            internalTarget = argPBlock.horizontalresizetarget
+
+        if argX > internalTarget:
+            tmpRatio = internalTarget / argX
+            newY = round(argY * tmpRatio)
+            return (internalTarget, newY)
+        else:
+            return (argX, argY)
+
+def sortDict(argSourceDict: dict) -> dict:
+    sortedDict = dict()
+    sortedKeys = sorted(argSourceDict, key=argSourceDict.get, reverse=True)
+    for w in sortedKeys:
+        sortedDict[w] = argSourceDict[w]
+    return sortedDict
+
+def cropUniversal(argImgArray: numpy.ndarray, argPBlock, argVertical: bool, argExhaustive: bool, argReverse: bool):
+    emptyLinesList = list()
+
+    internalAxis1, internalAxis2 = int(not argVertical), int(argVertical)    
+
+    if argExhaustive:
+        internalRange = range(0, numpy.size(argImgArray, internalAxis1))
+        internalSize1 = numpy.size(argImgArray, internalAxis1)
+    else:
+        if argReverse:
+            internalRange = range(numpy.size(argImgArray, internalAxis1) - 1, -1, -1)
+        else:
+            internalRange = range(0, numpy.size(argImgArray, internalAxis1))
+    internalSize2 = numpy.size(argImgArray, internalAxis2)   
+
+    for coord in internalRange:
+        if argVertical:
+            vals, freq = numpy.unique(argImgArray[coord, :], return_counts=True)
+        else:
+            vals, freq = numpy.unique(argImgArray[:, coord], return_counts=True)
+        linePixelDict = dict(zip(vals, freq))
+        linePixelDictSorted = sortDict(linePixelDict)
+        lineFuzzyError = round((1 - fuzzyCount(linePixelDictSorted, argPBlock) / internalSize2) * 100, 4)
+        if lineFuzzyError < argPBlock.fillratio:
+            emptyLinesList.append(coord)
+        else:
+            if not argExhaustive:
+                return emptyLinesList, lineFuzzyError
+            
+    if argExhaustive:
+        if len(emptyLinesList) == internalSize1:
+            emptyLinesList.clear()
+    else:
+        emptyLinesList.clear()
+    return emptyLinesList, -1
+
+def workerEntryPoint(argWorkerArgs):
+    argImageFilePath, wkArgs = argWorkerArgs
+    errorMessagesList = list()
+    debugMessagesList = list()
+
+    try:
+        im = Image.open(argImageFilePath)
+    except:
+        errorMessagesList.append(f"Error opening {argImageFilePath}.")
+        return argImageFilePath, debugMessagesList, errorMessagesList
+    
+    debugMessagesList.append(f"Cropping {argImageFilePath.as_posix()}")
+    debugMessagesList.append(f"{im.format} {im.size} {im.mode}")
+    
+    if "icc_profile" in im.info:
+        debugMessagesList.append("Applying color profile.")
+        f = io.BytesIO(im.info["icc_profile"])
+        im = ImageCms.profileToProfile(im, ImageCms.ImageCmsProfile(f), ImageCms.createProfile("sRGB"), outputMode="RGB")
+
+    imArray = numpy.asarray(im)
+    if im.mode == "L":
+        imWkArray = numpy.copy(imArray)
+    else:
+        imWkArray = numpy.asarray(im.convert(mode="L", dither=Image.Dither.NONE))
+
+    topLinesList, topFuzzyError = cropUniversal(imWkArray, wkArgs, True, False, False)
+    bottomLinesList, bottomFuzzyError = cropUniversal(imWkArray, wkArgs, True, False, True)
+
+    if len(topLinesList) == 0:
+        if topFuzzyError == -1:
+            debugMessagesList.append("Fast crop (top): probably empty image")
+        else:
+            debugMessagesList.append(f"Fast crop (top): nothing cropped, Fr: {topFuzzyError}%")
+    else:
+        debugMessagesList.append(f"Fast crop (top): content line {topLinesList[-1]}, Fe: {topFuzzyError}%")
+    if len(bottomLinesList) == 0:
+        if bottomFuzzyError == -1:
+            debugMessagesList.append("Fast crop (bottom): probably empty image")
+        else:
+            debugMessagesList.append(f"Fast crop (bottom): nothing cropped, Fr: {bottomFuzzyError}%")  
+    else:
+        debugMessagesList.append(f"Fast crop (bottom): content line {bottomLinesList[-1]}, Fr: {bottomFuzzyError}%")
+    imArray = numpy.delete(imArray, topLinesList + bottomLinesList, 0)
+
+    leftLinesList, leftFuzzyError = cropUniversal(imWkArray, wkArgs, False, False, False)
+    rightLinesList, rightFuzzyError = cropUniversal(imWkArray, wkArgs, False, False, True)
+
+    if ((len(leftLinesList) + len(rightLinesList)) / im.size[0]) * 100 > wkArgs.exhaustivethreshold:
+        if len(leftLinesList) == 0:
+            if leftFuzzyError == -1:
+                debugMessagesList.append("Fast crop (left): probably empty image")
+            else:
+                debugMessagesList.append(f"Fast crop (left): nothing cropped, Fr: {leftFuzzyError}%")  
+        else:
+            debugMessagesList.append(f"Fast crop (left): content line {leftLinesList[-1]}, Fr: {leftFuzzyError}%")
+        if len(rightLinesList) == 0:
+            if leftFuzzyError == -1:
+                debugMessagesList.append("Fast crop (right): probably empty image")
+            else:
+                debugMessagesList.append(f"Fast crop (right): nothing cropped, Fr: {rightFuzzyError}%")  
+        else:
+            debugMessagesList.append(f"Fast crop (right): content line {rightLinesList[-1]}, Fr: {rightFuzzyError}%")
+        imArray = numpy.delete(imArray, leftLinesList + rightLinesList, 1)
+    else:
+        lineListH, dummyFuzzyError = cropUniversal(imWkArray, wkArgs, False, True, False)
+        debugMessagesList.append(f"Exhaustive crop (horizontal): {len(lineListH)} lines matched")
+        imArray = numpy.delete(imArray, lineListH, 1)
+
+    imCrop = Image.fromarray(imArray, im.mode)
+    if im.mode == "P":
+        imCrop.putpalette(im.getpalette())
+
+    if wkArgs.noresize:
+        imResize = imCrop
+    else:
+        imResize = imCrop.resize(getResampleSize(imCrop.size[0], imCrop.size[1], wkArgs), Image.Resampling.LANCZOS)
+
+    currentResultPng = getResultFilePath(argImageFilePath, "png", wkArgs)
+    os.makedirs(currentResultPng.parent, exist_ok=True)
+    savePng(imResize, currentResultPng, wkArgs)
+
+    if wkArgs.encodeavif:
+        currentResultAvif = getResultFilePath(argImageFilePath, "avif", wkArgs)
+        os.makedirs(currentResultAvif.parent, exist_ok=True)
+        encodeProcess = subprocess.run(getAvifCmdline(currentResultPng, currentResultAvif, wkArgs), \
+                                       stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, creationflags=subprocess.BELOW_NORMAL_PRIORITY_CLASS)
+        if encodeProcess.returncode != 0:
+            errorMessagesList.append("Possible error in AVIF encoding")
+            errorMessagesList.append(f"Avifenc stderr: {encodeProcess.stderr}")
+        else:
+            debugMessagesList.append("Encoded to AVIF")
+
+    return argImageFilePath, debugMessagesList, errorMessagesList
+
+def main():
+    imageFileList = list()
+    args = getParameterBlock()
+
+    try:
+        logging.basicConfig(filename=args.logpath, format="%(asctime)s %(levelname)s %(message)s", encoding="utf-8", level=logging.DEBUG)
+    except FileNotFoundError:
+        print("Can't setup logging due to invalid path. Logging is disabled.")
+
+    print("Autocrop script R4 is starting")
+    logging.info("Autocrop script R4 is starting")
+
+    logging.info(f"Line threshold: {str(args.fillratio)}%")
+    logging.info(f"Fuzzy distance: {str(args.colordistance)} colors")
+    logging.info(f"Input folder: {args.input.as_posix()}")
+    logging.info(f"Output template: {args.output.as_posix()}")
+
+    if pathlib.Path.is_file(args.input):
+        logging.info("Single file mode")
+        imageFileList.append((args.input, args))
+    else:
+        for currentImageFilePath in args.input.rglob("*"):
+            if currentImageFilePath.is_file and \
+                    (currentImageFilePath.suffix == ".jpg" or currentImageFilePath.suffix == ".jpeg" or currentImageFilePath.suffix == ".png"):
+                imageFileList.append((currentImageFilePath, args))
+        logging.info(f"Converting {len(imageFileList)} files")
+    
+    gotErrors = False
+    with multiprocessing.Pool() as wkPool:
+        wkResults = wkPool.imap_unordered(workerEntryPoint, imageFileList)
+        for wkFile, wkDebugMessagesList, wkErrorMesssagesList in wkResults:
+            if len(wkErrorMesssagesList) != 0:
+                gotErrors = True
+            print(f"Converting {wkFile.as_posix()}")
+            for currentDebugLine in wkDebugMessagesList:   
+                logging.info(currentDebugLine)
+            for currentErrorLine in wkErrorMesssagesList:
+                logging.error(currentErrorLine)
+
+    if gotErrors:
+        print("!! Errors may have occured. Please check log.")
+
+    logging.info("Autocrop is finished")
+    print("Autocrop is finished")
+
+    if not args.nowait:
+        input("Press enter to exit.")
+    
+if __name__ == "__main__":
+    main()
